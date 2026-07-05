95e028db0cdc9571cdf5877464bdbba472927fee
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-07-05 18:41:03 +0700

    Create index.ts

---
+++ apps/web/src/applications/Reviews/index.ts
@@ -0,0 +1,5 @@
+export { default } from "./ReviewsApp";
+export { ReviewsWindow } from "./ReviewsWindow";
+export { useReviewsData } from "./ReviewsHooks";
+export { ReviewsAPI } from "./ReviewsAPI";
+export type { ReviewsViewState } from "./ReviewsTypes";
