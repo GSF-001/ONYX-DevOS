659e31d8c872c4e0623b958f03db591a339b6375
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-07-05 18:11:14 +0700

    Create Talking.tsx

---
+++ apps/web/src/mascot/Talking.tsx
@@ -0,0 +1,10 @@
+// mascot/Talking.tsx
+export function TalkingFace() {
+  return (
+    <g>
+      <circle cx="10" cy="14" r="1.4" fill="#000" />
+      <circle cx="22" cy="14" r="1.4" fill="#000" />
+      <ellipse cx="16" cy="21" rx="4" ry="3" fill="#000" />
+    </g>
+  );
+}
