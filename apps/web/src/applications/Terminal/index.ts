d1437da58cabd8d77220008fefb33b9d890f2292
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-07-05 23:24:43 +0700

    Create index.ts

---
+++ apps/web/src/applications/Terminal/index.ts
@@ -0,0 +1,4 @@
+export { default } from "./TerminalApp";
+export { TerminalWindow } from "./TerminalWindow";
+export { TerminalAPI } from "./TerminalAPI";
+export { loadPersistedHistory, appendPersistedHistory, clearPersistedHistory } from "./TerminalStore";
