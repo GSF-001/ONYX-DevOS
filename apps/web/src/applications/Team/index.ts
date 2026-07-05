b5f38b258d2b09c018d5b3569444bdcb44a1cddd
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-07-05 23:05:41 +0700

    Create index.ts

---
+++ apps/web/src/applications/Team/index.ts
@@ -0,0 +1,5 @@
+export { default } from "./TeamApp";
+export { TeamWindow } from "./TeamWindow";
+export { useTeamData } from "./TeamHooks";
+export { TeamAPI } from "./TeamAPI";
+export type { TeamViewState } from "./TeamTypes";
