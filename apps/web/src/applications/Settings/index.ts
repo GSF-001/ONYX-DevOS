ee4e246fd77074590047f1b1701812f2936fc4f7
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-07-05 23:29:32 +0700

    Create index.ts

---
+++ apps/web/src/applications/Settings/index.ts
@@ -0,0 +1,4 @@
+export { default } from "./SettingsApp";
+export { SettingsWindow } from "./SettingsWindow";
+export { useSettings } from "./SettingsHooks";
+export type { SettingsState, NotificationPrefs } from "./SettingsTypes";
